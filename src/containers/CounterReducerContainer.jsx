ec8770ea6da21cb3389830c05cf275728e197f38
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-06-29 14:18:23 +0000

    feat: adding reducers and CounterReducerContainer

--- src/containers/CounterReducerContainer.jsx
+++ src/containers/CounterReducerContainer.jsx
@@ -1,10 +1,87 @@
-import React from "react";
-import { Typography } from "@mui/material";
+// Karena sudah pasti menggunakan reducer, kita gunakan useReducer
+import React, { useReducer } from "react";
+
+import { Box, Button, TextField, Typography } from "@mui/material";
+
+// import reducernya di sini
+import {
+  counterInitialValue,
+  CounterReducer,
+} from "../reducers/CounterReducer.js";
 
 const CounterReducerContainer = () => {
+  const [counter, dispatchCounter] = useReducer(
+    CounterReducer,
+    counterInitialValue
+  );
+
+  const buttonDecrementOnClickHandler = () => {
+    dispatchCounter({
+      tipeAksi: "decrement",
+    });
+  };
+
+  const buttonIncrementOnClickHandler = () => {
+    dispatchCounter({
+      tipeAksi: "increment",
+    });
+  };
+
+  const buttonResetOnClickHandler = () => {
+    dispatchCounter({
+      tipeAksi: "reset",
+    });
+  };
+
   return (
     <>
-      <Typography variant="h5">Counter Reducer</Typography>
+      <Box
+        sx={{
+          border: "1px dashed grey",
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <Typography variant="body1" component="div">
+          Use Reducer
+        </Typography>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <TextField
+            disabled
+            label="Current Counter"
+            // defaultValue="0"
+            value={counter}
+            size="small"
+          />
+        </Box>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={buttonDecrementOnClickHandler}
+          >
+            -1
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={buttonResetOnClickHandler}
+          >
+            0
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={buttonIncrementOnClickHandler}
+          >
+            +1
+          </Button>
+        </Box>
+      </Box>
     </>
   );
 };
---
+++ src/reducers/CounterReducer.js
@@ -0,0 +1,24 @@
+// ini hanyalah initial value, jadi tidak apa dibuat const
+export const counterInitialValue = 1;
+
+export const CounterReducer = (counter, action) => {
+  // asumsi: action memiliki tipeAksi sebagai kondisi
+  // tipeAksi memiliki string sebagai berikut:
+  // "increment" untuk menambahkan counter (+1)
+  // "decrement" untuk mengurangi counter (-1)
+  // "incrementSpec" untuk menambahkan counter sejumlah "amount" (+ amount)
+  // "decrementSpec" untuk mengurangi counter sejumlah "amount" (- amount)
+  // "reset" untuk membuat counter jadi 0
+
+  if (action.tipeAksi === "increment") {
+    return counter + 1;
+  } else if (action.tipeAksi === "decrement") {
+    return counter - 1;
+  } else if (action.tipeAksi === "incrementSpec") {
+    return counter + action.amount;
+  } else if (action.tipeAksi === "decrementSpec") {
+    return counter - action.amount;
+  } else if (action.tipeAksi === "reset") {
+    return 0;
+  }
+};
